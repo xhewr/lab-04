3dc518be491229e1a807c6b8e09858c96a3abd8f
xhewr <xhewr123@mailinator.com>
2017-03-14 16:22:45 -0700

    Update findMaximum.h

--- findMaximum.h
+++ findMaximum.h
@@ -3,9 +3,14 @@
 #ifndef FINDMAXIMUM_H
 #define FINDMAXIMUM_H
 
+//Template to find maximum value function
+//Accepts 2 parameters type T
+//Returns the largest 
+//Works with all types that have a < operator (int, double, char, string)
 template <class T>
-T findMaximum(T number1, T number2);
-
-#include "findMaximum.cpp" 
+T findMaximum(T number1, T number2)
+{
+	return (number1<number2) ? number2 : number1;
+}
 
 #endif
