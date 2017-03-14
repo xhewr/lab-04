e95b18c666269f81cba8b782c28748c642ca9003
xhewr <xhewr123@mailinator.com>
2017-03-14 16:24:39 -0700

    Update total.h

--- total.h
+++ total.h
@@ -3,9 +3,22 @@
 #ifndef TOTAL_H
 #define TOTAL_H
 
+//Template to total value function
+//Will return a type T and takes an int as argument
+//T cannot be a string because strings can't be set to 0
+//cin needs to support type T
 template <typename T>
-T total(int numInputs);
-
-#include "total.cpp" 
+T total(int numInputs)
+{
+	T temp = 0;
+	T runningTotal = 0;
+	for (int count = 0; count < numInputs; count++)
+	{
+		cout << "Enter value: ";
+		cin >> temp;
+		runningTotal += temp;
+	}
+	return runningTotal;
+}
 
 #endif
