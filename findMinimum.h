397a67cb317e383b58783c2d6cb4da0355d40b3b
xhewr <xhewr123@mailinator.com>
2017-03-14 16:23:39 -0700

    Update findMinimum.h

--- findMinimum.h
+++ findMinimum.h
@@ -3,9 +3,15 @@
 #ifndef FINDMINIMUM_H
 #define FINDMINIMUM_H
 
-template <class T>
-T findMinimum(T number1, T number2);
+//Template to find minimum value function
+//Accepts 2 parameters of type T
+//Returns the smallest
+//Works with all types that have a < operator (int, double, char, string)
 
-#include "findMinimum.cpp" 
+template <class T>
+T findMinimum(T number1, T number2)
+{
+	return (number1 < number2) ? number1 : number2;
+}
 
 #endif
