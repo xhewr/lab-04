b2f177d42f3efedda4c9d510e1a00806780a8990
xhewr <xhewr123@mailinator.com>
2017-03-14 16:21:41 -0700

    Update findAbsolute.h

--- findAbsolute.h
+++ findAbsolute.h
@@ -3,9 +3,18 @@
 #ifndef FINDABSOLUTE_H
 #define FINDABSOLUTE_H
 
+//Template to absolute value function
+//Accepts single parameter of type T
+//Returns the absolute value for the passed value 
+//Worls with all types that can be compared with a "< 0" and have a - operator (not strings)
 template <class T>
-T findAbsoluteValue(T args);
-
-#include "findAbsolute.cpp" 
+T findAbsoluteValue(T args)
+{
+	if (args < 0)
+	{
+		args = -args;
+	}
+	return args;
+}
 
 #endif
